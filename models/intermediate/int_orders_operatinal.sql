c02319639499d3359142377fa6cab852431fdb99
Zehra Dedebali <zehradedebali@gmail.com>
2025-12-03 14:01:36 +0000

    değişiklik

---
+++ models/intermediate/int_orders_operatinal.sql
@@ -0,0 +1,16 @@
+select
+    o.orders_id,
+    o.date_date,
+    round(
+        (o.margin + s.shipping_fee - s.logcost - s.ship_cost), 2
+    ) as operational_margin,
+    o.revenue,
+    o.quantity,
+    o.purchase_cost,
+    o.margin,
+    s.shipping_fee,
+    s.ship_cost,
+    s.logcost
+from {{ref("int_orders_operatinal")}} o
+left join {{ref("stg_raw__ship")}} s using (orders_id)
+order by orders_id desc
---
+++ models/intermediate/new.sql
@@ -0,0 +1,17 @@
+select
+    o.orders_id,
+    o.date_date,
+    round(
+        (o.margin + s.shipping_fee - s.logcost - s.ship_cost), 2
+    ) as operational_margin,
+    o.revenue,
+    o.quantity,
+    o.purchase_cost,
+    o.margin,
+    s.shipping_fee,
+    s.ship_cost,
+    s.logcost
+from {{ (int_orders_operatinal) }} o
+left join {{ (stg_raw__ship) }} s using (orders_id)
+order by orders_id desc
+;
